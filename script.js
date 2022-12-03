49038138009a9fd113e8084b39aa66582909ee1c
George <georgemeleancaa@gmail.com>
2022-12-03 15:13:25 +0200

    Change table color to dark

--- script.js
+++ script.js
@@ -4,10 +4,19 @@ function switchTheme() {
 
   let card = document.getElementsByClassName("card");
 
+  let table = document.getElementsByTagName("table")[0];
+  let tr = document.getElementsByTagName("tr");
+
   body.classList.add("bg-dark");
   nav.classList.add("bg-dark", "navbar-dark");
 
   for (let cards of card) {
     cards.classList.add("text-white", "bg-dark", "border-light");
   }
+
+  table.style.backgroundColor = "black";
+  for (let tre of tr) {
+    tre.style.color = "white";
+  }
+  
 }

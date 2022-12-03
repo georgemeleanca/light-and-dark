cefee4e321901b084bc05306743cedbd44a3dbfc
George <georgemeleancaa@gmail.com>
2022-12-03 14:46:45 +0200

    Nav color to dark

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function switchTheme() {
   let body = document.getElementsByTagName("body")[0];
+  let nav = document.getElementsByTagName("nav")[0];
   body.classList.add("bg-dark");
+  nav.classList.add("bg-dark", "navbar-dark");
 }

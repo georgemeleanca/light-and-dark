1f475251c05285e6c6901a4f92440bfcae0dec91
George <georgemeleancaa@gmail.com>
2022-12-03 16:07:28 +0200

    Change background color to light

--- script.js
+++ script.js
@@ -1,33 +1,38 @@
 function switchTheme() {
+
+  let button = document.getElementById("themeSwitch");
   let body = document.getElementsByTagName("body")[0];
   let nav = document.getElementsByTagName("nav")[0];
-
   let cards = document.getElementsByClassName("card");
-
   let table = document.getElementsByTagName("table")[0];
   let tr = document.getElementsByTagName("tr");
 
-  body.classList.add("bg-dark");
-  nav.classList.add("bg-dark", "navbar-dark");
+  if (button.innerHTML === "Activate Dark Theme") {
+    body.classList.add("bg-dark");
+    nav.classList.add("bg-dark", "navbar-dark");
 
-  for (let card of cards) {
-    card.classList.add("text-white", "bg-dark", "border-light");
-    card.parentElement.classList.add("col-md-12");
+    for (let card of cards) {
+      card.classList.add("text-white", "bg-dark", "border-light");
+      card.parentElement.classList.add("col-md-12");
+    }
+
+    table.style.backgroundColor = "black";
+    for (let tre of tr) {
+      tre.style.color = "white";
+    }
+
+    let footer = document.getElementsByClassName("navbar")[document.getElementsByClassName("navbar").length - 1];
+    footer.style.float = "right";
+    footer.style.color = "white";
+
+    let header = document.getElementsByTagName("h1")[0];
+    header.innerHTML = "This is the Dark theme";
+    header.style.color = "white";
+
+
+    button.innerHTML = "Activate Light Theme";
   }
-
-  table.style.backgroundColor = "black";
-  for (let tre of tr) {
-    tre.style.color = "white";
+  else {
+    body.classList.remove("bg-dark");
   }
-
-  let footer = document.getElementsByClassName("navbar")[ document.getElementsByClassName("navbar").length-1];
-  footer.style.float = "right";
-  footer.style.color = "white";
-
-  let header = document.getElementsByTagName("h1")[0];
-  header.innerHTML = "This is the Dark theme";
-  header.style.color = "white";
-
-  let button = document.getElementById("themeSwitch");
-  button.innerHTML = "Activate Light Theme";
 }

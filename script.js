72255d39ded0bdbb6429f6349c13fad7678c4baa
George <georgemeleancaa@gmail.com>
2022-12-03 14:49:42 +0200

    Card color to dark

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function switchTheme() {
   let body = document.getElementsByTagName("body")[0];
   let nav = document.getElementsByTagName("nav")[0];
+
+  let card = document.getElementsByClassName("card");
+
   body.classList.add("bg-dark");
   nav.classList.add("bg-dark", "navbar-dark");
+
+  for (let cards of card) {
+    cards.classList.add("text-white", "bg-dark", "border-light");
+  }
 }
